67799addecffdf29112d333fddc636eebec6d755
Afshar Sharifi <afsharsharifi6@gmail.com>
2021-10-09 16:38:38 +0330

    JS File Structuring

---
+++ main.js
@@ -0,0 +1,4 @@
+function AddNewTask() {
+    var allTask = document.getElementById("all-tasks")
+    allTask.innerHTML += '<div class="todo-task"><input type="checkbox"> <input type="text"></input><br></div>'
+}
