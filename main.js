0ed1191c061a510012c8a8df867fb2e1f59a91d5
Afshar Sharifi <afsharsharifi6@gmail.com>
2021-10-10 10:03:10 +0330

    re-build

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+var allTask = document.getElementById("ongoing-task")
+if (allTask.innerHTML == "") {
+    document.write("No Task Added")
+}
+
 function AddNewTask() {
-    var allTask = document.getElementById("all-tasks")
-    allTask.innerHTML += '<div class="todo-task"><input type="checkbox"> <input type="text"></input><br></div>'
+    var allTask = document.getElementById("ongoing-task")
+    var newTaskTitle = document.getElementById("txtNewTaskTitle").value
+    if (newTaskTitle != "") {
+        allTask.innerHTML += '<a href="#"><li class="todo-task">' + newTaskTitle + '</li></a>'
+        document.getElementById("txtNewTaskTitle").value = ""
+    }
 }
