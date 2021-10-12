ac6c39175a8c783f5198dce09b4dc1b9385f95d8
Afshar Sharifi <afsharsharifi6@gmail.com>
2021-10-12 13:12:12 +0330

    Logic is working until here

--- main.js
+++ main.js
@@ -1,17 +1,73 @@
-var allTask = document.getElementById("ongoing-task")
-if (allTask.innerHTML == "") {
-    document.write("No Task Added")
-}
+const inputBox = document.querySelector(".InputFiled input")
+const addBtn = document.querySelector(".InputFiled button")
+const todoList = document.querySelector(".todoList")
+const deleteAllBtn = document.querySelector(".footer button")
 
-function AddNewTask() {
-    var allTask = document.getElementById("ongoing-task")
-    var newTaskTitle = document.getElementById("txtNewTaskTitle").value
-    if (newTaskTitle != "") {
-        allTask.innerHTML += '<a href="#" class="a-task col s12 btn pink darken-2"><li class="todo-task left-align">' + newTaskTitle + '</li></a>'
-        document.getElementById("txtNewTaskTitle").value = ""
+
+inputBox.onkeyup = () => {
+    let userData = inputBox.value
+
+    if (userData.trim() != 0) {
+        document.querySelector(".InputFiled button").disabled = false
+    } else {
+        document.querySelector(".InputFiled button").disabled = true
     }
 }
 
-function DoneThisTask() {
-    var this_task = document.getElementsByClassName("todo-task")
+
+ShowTask()
+
+
+addBtn.onclick = () => {
+    let userData = inputBox.value
+    let getLocalStorage = localStorage.getItem("New Todo")
+    if (getLocalStorage == null) {
+        listArr = []
+    } else {
+        listArr = JSON.parse(getLocalStorage)
+    }
+    listArr.push(userData)
+    localStorage.setItem("New Todo", JSON.stringify(listArr))
+    ShowTask()
 }
+
+function ShowTask() {
+    let getLocalStorage = localStorage.getItem("New Todo")
+    if (getLocalStorage == null) {
+        listArr = []
+    } else {
+        listArr = JSON.parse(getLocalStorage)
+    }
+    const pendingNum = document.querySelector(".pendingTask")
+    pendingNum.textContent = listArr.length
+
+
+    let newLiTag = ''
+    listArr.forEach((item, index) => {
+        newLiTag += `<li> ${item} <span onclick = "DoneTask(${index})"><i class="tasks material-icons right">check</i></span></li>`
+    });
+    todoList.innerHTML = newLiTag
+    inputBox.value = ""
+    document.querySelector(".InputFiled button").disabled = true
+}
+
+function DoneTask(index) {
+    let getLocalStorage = localStorage.getItem("New Todo")
+    listArr = JSON.parse(getLocalStorage)
+    listArr.splice(index, 1)
+    localStorage.setItem("New Todo", JSON.stringify(listArr))
+    ShowTask()
+}
+
+deleteAllBtn.onclick = () => {
+    listArr = []
+    localStorage.setItem("New Todo", JSON.stringify(listArr))
+    ShowTask()
+}
+
+document.getElementById("txtNewTask").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        document.getElementById("btnAddTask").click()
+        return false;
+    }
+});
